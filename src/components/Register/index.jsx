ec944a665b363cbd2b11a4f2b4e6546c177e9c78
Achmad Maulana Alawi <alawitch.b@gmail.com>
2024-06-11 17:12:38 +0700

    add .env to gitignore

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,40 +1,43 @@
-import { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import { toast } from "react-toastify";
-import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { register } from "../../redux/actions/auth";
-import GoogleLogin from "../GoogleLogin";
+import { useState } from "react"
+import Button from "react-bootstrap/Button"
+import Form from "react-bootstrap/Form"
+import { toast } from "react-toastify"
+import { useNavigate } from "react-router-dom"
+import { useDispatch } from "react-redux"
+import { register } from "../../redux/actions/auth"
+import GoogleLogin from "../GoogleLogin"
 
 function Register() {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [photo, setPhoto] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const [photo, setPhoto] = useState()
+  const [isLoading, setIsLoading] = useState(false)
 
   const onSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     if (password != confirmPassword) {
-      toast.error(`Password and confirm password must be same!`);
-      return;
+      toast.error(`Password and confirm password must be same!`)
+      return
     }
 
     // dispatch the register action
-    dispatch(register(navigate, email, password, name, photo, setIsLoading));
-  };
+    dispatch(register(navigate, email, password, name, photo, setIsLoading))
+  }
 
   return (
     <>
       <h1 className="fw-semibold mb-5">Register</h1>
       <Form onSubmit={onSubmit}>
-        <Form.Group className="mb-3" controlId="name">
+        <Form.Group
+          className="mb-3"
+          controlId="name"
+        >
           <Form.Label>Name *</Form.Label>
           <Form.Control
             type="text"
@@ -44,7 +47,10 @@ function Register() {
             required
           />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="email">
+        <Form.Group
+          className="mb-3"
+          controlId="email"
+        >
           <Form.Label>Email address *</Form.Label>
           <Form.Control
             type="email"
@@ -54,7 +60,10 @@ function Register() {
             required
           />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="password">
+        <Form.Group
+          className="mb-3"
+          controlId="password"
+        >
           <Form.Label>Password *</Form.Label>
           <Form.Control
             type="password"
@@ -64,7 +73,10 @@ function Register() {
             required
           />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="confirmPassword">
+        <Form.Group
+          className="mb-3"
+          controlId="confirmPassword"
+        >
           <Form.Label>Confirm Password *</Form.Label>
           <Form.Control
             type="password"
@@ -74,22 +86,16 @@ function Register() {
             required
           />
         </Form.Group>
-        <Form.Group controlId="photo" className="mb-3">
+        <Form.Group
+          controlId="photo"
+          className="mb-3"
+        >
           <Form.Label>Photo</Form.Label>
           <Form.Control
             type="file"
             onChange={(e) => setPhoto(e.target.files[0])}
           />
         </Form.Group>
-        <div className="input-group mb-3">
-          <label className="input-group-text" htmlFor="inputGroupSelect01">
-            Role
-          </label>
-          <select className="form-select" id="inputGroupSelect01">
-            <option value="1">Admin</option>
-            <option value="2">User</option>
-          </select>
-        </div>
         <Button
           className="w-100"
           variant="primary"
@@ -111,7 +117,7 @@ function Register() {
         </a>
       </p>
     </>
-  );
+  )
 }
 
-export default Register;
+export default Register
